68f7e0f2d6826b2060272f0218398f453444caa8
Mahdiyar <mahdiyarsne98@gmail.com>
2025-05-24 22:59:34 +0330

    لیست نشان دادن پست اضافه شد

--- admin/includes/admin_navgation.php
+++ admin/includes/admin_navgation.php
@@ -39,7 +39,7 @@
                  <a href="javascript:;" data-toggle="collapse" data-target="#post_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                  <ul id="post_dropdown" class="collapse">
                      <li>
-                         <a href="#">View All Posts</a>
+                         <a href="./posts.php">View All Posts</a>
                      </li>
                      <li>
                          <a href="#">Add Post</a>
@@ -48,7 +48,7 @@
              </li>
 
              <li>
-                 <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+                 <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
              </li>
 
              <li class="active">
