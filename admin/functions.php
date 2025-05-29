a7e4dcc40fa21d00eb7aa08b403c3e8b80c08c65
Mahdiyar <mahdiyarsne98@gmail.com>
2025-05-29 22:12:49 +0330

    ویرایش پست اتمام شد

--- admin/functions.php
+++ admin/functions.php
@@ -4,6 +4,15 @@
 
 
 
+function comfirm($result)
+{
+    global $connection;
+    if (!$result) {
+        die("QUERY FAILED" . mysqli_error($connection));
+    }
+}
+
+
 function insert_categories()
 {
 
--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -5,7 +5,7 @@ if (isset($_GET['p_id'])) {
     $the_post_id = $_GET['p_id'];
 }
 
-$query = "SELECT * FROM posts";
+$query = "SELECT * FROM posts WHERE post_id=$the_post_id ";
 $select_post_by_id = mysqli_query($connection, $query);
 
 while ($row = mysqli_fetch_assoc($select_post_by_id)) {
@@ -17,8 +17,40 @@ while ($row = mysqli_fetch_assoc($select_post_by_id)) {
     $post_image = $row["post_image"];
     $post_author = $row["post_author"];
     $post_status = $row["post_status"];
-    $post_category_id = $row["post_category_id"];
-    $post_comment_count = $row["post_comment_count"];
+}
+
+if (isset($_POST['update_post'])) {
+
+    $post_title = $_POST['title'];
+    $post_category_id = $_POST['post_category'];
+    $post_author = $_POST['post_author'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_tags = $_POST['post_tags'];
+    $post_content = $_POST['post_content'];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    if (empty($post_image)) {
+        $query = "SELECT * FROM posts WHERE post_id =$the_post_id";
+    }
+
+
+    $query = "UPDATE posts SET ";
+    $query .= "post_title = '{$post_title}', ";
+    $query .= "post_author = '{$post_author}', ";
+    $query .= "post_category_id = '{$post_category_id}', ";
+    $query .= "post_status = '{$post_status}', ";
+    $query .= "post_tags = '{$post_tags}', ";
+    $query .= "post_content = '{$post_content}', ";
+    $query .= "post_image = '{$post_image}' ";
+    $query .= "WHERE post_id = {$the_post_id} ";
+
+    $update_query = mysqli_query($connection, $query);
+    if (!$update_query) {
+        die("QUERY FAILED" . mysqli_error($connection));
+    }
 }
 
 
@@ -35,8 +67,23 @@ while ($row = mysqli_fetch_assoc($select_post_by_id)) {
 
 
     <div class="form-group">
-        <label for="post_category_id">Post Category Id</label>
-        <input type="text" value="<?php echo $post_category_id; ?>" class="form-control" name="post_category_id">
+
+        <select name="post_category" id="post_category">
+
+            <?php
+
+            $query = "SELECT * FROM categories";
+            $select_categories = mysqli_query($connection, $query);
+
+            while ($row = mysqli_fetch_assoc($select_categories)) {
+                $cat_id = $row["cat_id"];
+                $cat_title = $row["cat_title"];
+
+                echo "<option value='{$cat_id}'>{$cat_title}</option>";
+            }
+
+            ?>
+        </select>
     </div>
 
 
@@ -54,7 +101,7 @@ while ($row = mysqli_fetch_assoc($select_post_by_id)) {
 
 
     <div class="form-group">
-        <label for="image">Post Image</label>
+        <img width="100" src="../images/<?php echo $post_image ?>" alt="">
         <input type="file" name="image">
     </div>
 
@@ -72,7 +119,7 @@ while ($row = mysqli_fetch_assoc($select_post_by_id)) {
 
 
     <div class="form-group">
-        <input type="submit" class="btn btn-primary" name="edit_post" value="Edit Post">
+        <input type="submit" class="btn btn-primary" name="update_post" value="Update Post">
     </div>
 
 </form>
