1b8e900269f9bc087d9ed02988633fb361f78ef7
Mahdiyar <mahdiyarsne98@gmail.com>
2025-06-01 21:53:11 +0330

    صفحه دیدن پست ها اتمام شد

--- index.php
+++ index.php
@@ -16,42 +16,37 @@
 
             while ($row = mysqli_fetch_assoc($select_post)) {
 
+                $post_id = $row["post_id"];
+
                 $post_title = $row["post_title"];
                 $post_author = $row["post_author"];
                 $post_date = $row["post_date"];
                 $post_content = $row["post_content"];
                 $post_image = $row["post_image"];
-                ?>
+            ?>
 
 
 
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
 
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="post.php?p_id=<?php echo $post_id ?>"><?php echo $post_title; ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author; ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?> </p>
+                <hr>
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                <hr>
+                <p><?php echo $post_content; ?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-
-
-
-
-
-            <h1 class="page-header">
-                Page Heading
-                <small>Secondary Text</small>
-            </h1>
-
-            <!-- First Blog Post -->
-            <h2>
-                <a href="#"><?php echo $post_title; ?></a>
-            </h2>
-            <p class="lead">
-                by <a href="index.php"><?php echo $post_author; ?></a>
-            </p>
-            <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?> </p>
-            <hr>
-            <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-            <hr>
-            <p><?php echo $post_content; ?></p>
-            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-            <hr>
+                <hr>
 
 
 
